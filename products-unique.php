6aae5c638e256f78c53a5466ba46ae313129b244
JK <jingoo@kfoodot.com>
2021-04-16 16:15:45 -0700

    removed comments

--- products-unique.php
+++ products-unique.php
@@ -2,13 +2,8 @@
 $title = "Unique Products";
 $cat_id = "unique";
 $string = file_get_contents("products.json");
-// echo $string;
 $products = json_decode($string);
-// echo $products->natural;
-// $cat_id;
-// foreach($products->{$cat_id} as $product) echo $product->title;
-// var_dump($json_products->natural);
-// echo $json_products[$cat_id];
+
 require("header.php");
 require("products-detail.php");
 require("footer.php");
